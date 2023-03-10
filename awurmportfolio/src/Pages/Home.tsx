0cb75dc175a6cfb216b09e9fefa412e732827940
andywurm <andaiyewurm@gmail.com>
2023-03-10 18:26:53 -0500

    background image

--- awurmportfolio/src/Pages/Home.tsx
+++ awurmportfolio/src/Pages/Home.tsx
@@ -4,6 +4,7 @@ import pfp from '../Img/Me.png'
 import { useNavigate } from "react-router-dom"
 import { Paper } from "@mui/material"
 import '../Styles/HomeStyles.css'
+import bkg from '../Img/HomeBkg.png'
 
 const Home = () => {
 
@@ -19,36 +20,47 @@ const Home = () => {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            style={{ height: "80vh", width: "75%", margin: "0 auto" }}
+            style={{ width: "100%" }}
         >
+            <div style={{ width: "75%", height: "65vh", margin: "0 auto" }}>
+                <div className="homeContainer">
 
-            <div className="homeContainer">
+                    <div className="homeInfo">
+                        <div className="allInfo">
+                            <div className="keep">
+                                <h1 className="myName">Andaiye Wurm</h1>
+                                <img className="verify" src={verified} alt='badge' />
+                                <h3 className="pronouns">(She/Her)</h3>
+                            </div>
+                            <h2 className="location"> New York, NY</h2>
 
-                <div className="homeInfo">
-                    <div className="allInfo">
-                        <div className="keep">
-                            <h1 className="myName">Andaiye Wurm</h1>
-                            <img className="verify" src={verified} alt='badge' />
-                            <h3 className="pronouns">(She/Her)</h3>
+                            <div className="homeText">Looking for work as a Software Engineer or Frontend Developer.</div>
                         </div>
-                        <h2 className="location"> New York, NY</h2>
-
-                        <div className="homeText">Looking for work as a Software Engineer or Frontend Developer.</div>
                     </div>
-                </div>
 
-                <div className="homeCard">
-                    <Paper elevation={3} className="paper">
-                        <div style={{ display: 'flex', flexDirection: "row", padding: "20px 20px" }}>
-                            <img className="pfp" src={pfp} alt="Me" />
-                        </div>
-                        <div style={{ display: 'flex', flexDirection: "row" }}>
-                            <button onClick={() => handleClick()}>+ Connect</button>
-                        </div>
-                    </Paper>
+                    <div className="homeCard">
+                        <Paper elevation={3} className="paper">
+                            <div style={{ display: 'flex', flexDirection: "row", padding: "20px 20px" }}>
+                                <img className="pfp" src={pfp} alt="Me" />
+                            </div>
+                            <div style={{ display: 'flex', flexDirection: "row" }}>
+                                <button onClick={() => handleClick()}>+ Connect</button>
+                            </div>
+                        </Paper>
+
+                    </div>
+
                 </div>
 
             </div>
+
+            <div style={{
+                height: "20vh",
+                backgroundImage: 'url(' + bkg + ')',
+                // backgroundSize: 'cover',
+                // backgroundRepeat: 'no-repeat',
+                imageRendering: "crisp-edges"
+            }}></div>
 
         </motion.div>
     )
