e8a940f3d54de558c399d4e1741d38bdf13a7ffc
andywurm <andaiyewurm@gmail.com>
2023-03-17 18:35:01 -0400

    Added About

--- src/PortfolioData/Projects.tsx
+++ src/PortfolioData/Projects.tsx
@@ -31,6 +31,26 @@ const projects: Projects[] = [
         projectDescription: "Allows users to track which Japanese shows or movies they have watched, are watching or plan to watch. The user is able to read up on the content, and decide which list they would like to add that show or movie to.",
         projectTech: "React, JavaScript, HTML/CSS, Node.js, Express.js",
         status: "2022"
+    },
+    {
+        projectName: "Hobbies Hub",
+        projectLogo: "HHLogo.png",
+        projectImg: "HHub.png",
+        projectDate: "Oct - December 2021",
+        projectRepository: "https://github.com/lbcmp/Hobby-Hub",
+        projectDescription: "Empowered connections based on matching users to others with similar hobbies.",
+        projectTech: "React, JavaScript, HTML/CSS",
+        status: "2021"
+    },
+    {
+        projectName: "Hey! Say! JUMP!",
+        projectLogo: "HSJLogo.png",
+        projectImg: "HSJsite.png",
+        projectDate: "June - July 2021",
+        projectRepository: "https://github.com/andywurm/HeySayJUMPWebsite",
+        projectDescription: "Served as promotion for the group Hey! Say! JUMP! Allowed users to keep up with their favorite member as well as 'purchase' merchandise.",
+        projectTech: "JavaScript, HTML/CSS",
+        status: "2021"
     }
 ]
 
